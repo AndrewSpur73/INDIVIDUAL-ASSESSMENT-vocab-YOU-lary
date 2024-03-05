0cbc350a1eddf923a737aa09a930d0d2b6072af3
Andrew <andrewspurlock@rocketmail.com>
2024-03-04 19:40:39 -0600

    setup complete. All files added and connected succesfully

---
+++ utils/clearDom.js
@@ -0,0 +1,11 @@
+// const clearDom = () => {
+//   document.querySelector('#author-store').innerHTML = '';
+//   document.querySelector('#book-store').innerHTML = '';
+//   document.querySelector('#order-store').innerHTML = '';
+//   document.querySelector('#store').innerHTML = '';
+//   document.querySelector('#add-button').innerHTML = '';
+//   document.querySelector('#form-container').innerHTML = '';
+//   document.querySelector('#view').innerHTML = '';
+// };
+
+// export default clearDom;
