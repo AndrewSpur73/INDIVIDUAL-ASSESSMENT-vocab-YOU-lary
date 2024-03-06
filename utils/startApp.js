80e9dd871dade940fba9595f93df74b02b42c4bf
Andrew <andrewspurlock@rocketmail.com>
2024-03-05 18:59:47 -0600

    delete completed and filter buttons created

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,7 +1,9 @@
 import { getVocab } from '../api/vocabData';
+import filterButtons from '../components/buttons/filterButtons';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
+import domEvents from '../events/domEvents';
 import navEvents from '../events/navEvents';
 // import domEvents from '../events/domEvents';
 // import formEvents from '../events/formEvents';
@@ -10,11 +12,11 @@ import { showVocab } from '../pages/vocab';
 const startApp = () => {
   domBuilder(); // BUILD THE DOM
   navBar(); // DYNAMICALLY ADD THE NAV
-  // domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
+  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
   // formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
-
+  filterButtons(); // Filter Buttons
   // Put all vocab on the DOM on App load
   getVocab().then((vocab) => showVocab(vocab));
 };
